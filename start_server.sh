a56c385673e1e3b2528a46918079c03bf2d2fe99
huy1480 <huy@g.clemson.edu>
2025-11-01 23:06:05 -0400

    update server start script

--- start_server.sh
+++ start_server.sh
@@ -1,8 +1,26 @@
+#!/bin/bash
+
+trap cleanup INT
+
+cleanup() {
+	echo ""
+	echo "Shutting down all services"
+	kill $(jobs -p) 2>/dev/null
+	wait
+	echo "Shut down"
+	exit 0
+}
+
 echo "Starting ollama";
 ollama run llama3 &
+
 echo "Starting client service";
 node ./backend/client-service/server.js &
+
 echo "Starting admin service";
 node ./backend/admin-service/server.js &
+
 echo "Starting frontend";
 npm start --prefix ./frontend
+
+wait
